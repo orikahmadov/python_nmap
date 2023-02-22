d1ec7aa4db1df164d9b8ab4e78b462377e8a8e75
Orkhan Ahmadov <55695784+orikahmadov@users.noreply.github.com>
2023-02-22 11:32:36 +0100

    added print commands

--- python_nmap.py
+++ python_nmap.py
@@ -3,6 +3,7 @@ import subprocess
 import socket
 from fpdf import FPDF
 
+
 def run_host_discovery(target):
     try:
         ip_address = socket.gethostbyname(target)
@@ -10,10 +11,13 @@ def run_host_discovery(target):
         # Assume target is already an IP address
         ip_address = target
 
+    print("Running host discovery...")
     nmap_command = f'nmap -sn {ip_address}'
     process = subprocess.Popen(nmap_command.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
+    print("Host discovery complete.")
     return output.decode()
+
 
 def run_port_scan(target):
     try:
@@ -22,10 +26,13 @@ def run_port_scan(target):
         # Assume target is already an IP address
         ip_address = target
 
+    print("Running port scan...")
     nmap_command = f'nmap -sS -p 1-65535 -T4 -v {ip_address}'
     process = subprocess.Popen(nmap_command.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
+    print("Port scan complete.")
     return output.decode()
+
 
 def run_version_detection(target):
     try:
@@ -34,10 +41,13 @@ def run_version_detection(target):
         # Assume target is already an IP address
         ip_address = target
 
+    print("Running version detection...")
     nmap_command = f'nmap -sS -sV {ip_address}'
     process = subprocess.Popen(nmap_command.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
+    print("Version detection complete.")
     return output.decode()
+
 
 def run_os_detection(target):
     try:
@@ -46,10 +56,13 @@ def run_os_detection(target):
         # Assume target is already an IP address
         ip_address = target
 
+    print("Running OS detection...")
     nmap_command = f'nmap -sS -O {ip_address}'
     process = subprocess.Popen(nmap_command.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
+    print("OS detection complete.")
     return output.decode()
+
 
 def run_traceroute(target):
     try:
@@ -58,10 +71,13 @@ def run_traceroute(target):
         # Assume target is already an IP address
         ip_address = target
 
+    print("Running traceroute...")
     nmap_command = f'nmap --traceroute {ip_address}'
     process = subprocess.Popen(nmap_command.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
+    print("Traceroute complete.")
     return output.decode()
+
 
 def run_script_scan(target):
     try:
@@ -70,10 +86,13 @@ def run_script_scan(target):
         # Assume target is already an IP address
         ip_address = target
 
+    print("Running script scan...")
     nmap_command = f'nmap -sS -sV -sC {ip_address}'
     process = subprocess.Popen(nmap_command.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
+    print("Script scan complete.")
     return output.decode()
+
 
 def generate_report(target, output_file):
     # Run the Nmap scans
@@ -87,27 +106,36 @@ def generate_report(target, output_file):
     # Generate the PDF report
     pdf = FPDF()
     pdf.add_page()
-    pdf.set_font('Arial', 'B', 16)
-    pdf.cell(0, 10, f'Nmap Scan Results for {target}', 0, 1)
-    pdf.set_font('Arial', '', 12)
-    pdf.multi_cell(0, 10, f'Host Discovery:\n\n{host_discovery_results}\n\n')
-    pdf.multi_cell(0, 10, f'Port Scan:\n\n{port_scan_results}\n\n')
-    pdf.multi_cell(0, 10, f'Version Detection:\n\n{version_detection_results}\n\n')
-    pdf.multi_cell(0, 10, f'OS Detection:\n\n{os_detection_results}\n\n')
-    pdf.multi_cell(0, 10, f'Traceroute:\n\n{traceroute_results}\n\n')
-    pdf.multi_cell(0, 10, f'Script Scan:\n\n{script_scan_results}\n\n')
-    pdf.output(output_file, 'F')
+    pdf.set_font("Arial", size=12)
+    pdf.cell(200, 10, txt="Nmap Scan Report", ln=1, align="C")
+    pdf.cell(200, 10, txt=f"Target: {target}", ln=2, align="C")
+    pdf.cell(200, 10, txt="Host Discovery Results", ln=3, align="C")
+    pdf.multi_cell(0, 5, txt=host_discovery_results)
+    pdf.cell(200, 10, txt="Port Scan Results", ln=4, align="C")
+    pdf.multi_cell(0, 5, txt=port_scan_results)
+    pdf.cell(200, 10, txt="Version Detection Results", ln=5, align="C")
+    pdf.multi_cell(0, 5, txt=version_detection_results)
+    pdf.cell(200, 10, txt="OS Detection Results", ln=6, align="C")
+    pdf.multi_cell(0, 5, txt=os_detection_results)
+    pdf.cell(200, 10, txt="Traceroute Results", ln=7, align="C")
+    pdf.multi_cell(0, 5, txt=traceroute_results)
+    pdf.cell(200, 10, txt="Script Scan Results", ln=8, align="C")
+    pdf.multi_cell(0, 5, txt=script_scan_results)
+    pdf.output(output_file)
+
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('target', help='Target host or IP address')
-    parser.add_argument('-o', '--output', help='Output file')
+    parser.add_argument("-t", "--target", help="Target IP or hostname")
+    parser.add_argument("-o", "--output", help="Output file name")
     args = parser.parse_args()
 
-    if args.output:
+    if args.target and args.output:
         generate_report(args.target, args.output)
     else:
-        generate_report(args.target, 'nmap_report.pdf')
+        print("Please specify a target and an output file name.")
+        print("Use -h or --help for more information.")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
+    
